696c42708225ed67218ec6237bfd66599ccc99cf
wisevx <leidouweiben@gmail.com>
2024-09-19 05:18:28 +0000

    9月　19日　 javascriptの導入

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelector("h1").textContent="自分の名前"
+document.querySelector("h2").textContent="nishio"
+
+//documentとは！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル (Docment Objects Model)の略で、
+webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//document.querySelector("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//docment.quarySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "自分の名前"
