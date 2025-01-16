89c5459d0d2f437a303d8baeab9343f3bd3551ca
wisevx <leidouweiben@gmail.com>
2025-01-16 05:17:27 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ function ClickCounterGame(){
     const gameContainer = document.getElementById("game-container");
 
     let button1 = document.createElement("button");
-    button1.textContent = "+1ボタン"
+    button1.textContent = "+1ボタン";
+
+    let button2 = document.createElement("button");
+    button2.textContent ="+10ボタン";
+
+    let button3 = document.createElement("button");
+    button3.textContent = "リセット";
 
     let counter = document.createElement("p");
     counter.textContent = count;
@@ -13,7 +19,19 @@ function ClickCounterGame(){
         counter.textContent = count;
     })
 
+    button2.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })
+
+    counter.addEventListener("click",function(){
+        count = 0;
+       counter.textContent = count;
+    })
+
     gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
 }
 
