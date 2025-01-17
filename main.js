b2c437574288ea443f6246b1c1934b58c2c9eb65
wisevx <leidouweiben@gmail.com>
2025-01-17 06:18:38 +0000

    数当てゲームの追加

--- main.js
+++ main.js
@@ -15,18 +15,24 @@ function ClickCounterGame(){
     counter.textContent = count;
 
     button1.addEventListener("click",function(){
+        if(count <= 99){
+            
         count++;
         counter.textContent = count;
+        }
     })
 
     button2.addEventListener("click",function(){
+        if(count <= 90){
         count = count + 10;
         counter.textContent = count;
+        }
     })
 
-    counter.addEventListener("click",function(){
+    button3.addEventListener("click",function(){
         count = 0;
        counter.textContent = count;
+        
     })
 
     gameContainer.appendChild(button1);
@@ -36,3 +42,19 @@ function ClickCounterGame(){
 }
 
 ClickCounterGame();
+
+function startNumberGuessGame(){
+    const gameContainer = document.getElementById("game-container");
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let message = document.createElement('p');
+    let input = document.createElement('input');
+    message.textContent = (randomNumber);
+    input.type = 'number';
+    input.placeholder = "好きな数字を入力してください(1-100)";
+
+    gameContainer.appendChild(message);
+    gameContainer.appendChild(input);
+    gameContainer.appendChild(button);
+}
+
+startNumberGuessGame();
