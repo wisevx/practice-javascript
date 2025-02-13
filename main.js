ccae30228804c9f64feae0a82af781a171dd1f92
wisevx <leidouweiben@gmail.com>
2025-02-13 05:22:20 +0000

    02_13

--- main.js
+++ main.js
@@ -1,47 +1,77 @@
-// // function ClickCounterGame(){
-//     let count = 0;
-//     const gameContainer = document.getElementById("game-container");
 
-//     let button1 = document.createElement("button");
-// //     button1.textContent = "+1ボタン";
+const select = document.createElement("select");
+    select.name = "games";
+    select.id = "game-select";
+    const options = [
+        { value: "", text: "--ゲームを選択してください--" },
+        { value: "clicker", text: "クリッカーゲーム" },
+        { value: "number-guess", text: "数あてゲーム" }
+    ];
+    options.forEach(optionData => {
+        const option = document.createElement("option");
+        option.value = optionData.value;
+        option.textContent = optionData.text;
+        select.appendChild(option);
+    })
+    gameSelect.appendChild(select);
 
-//     let button2 = document.createElement("button");
-//     button2.textContent ="+10ボタン";
+const gameContainer = document.getElementById('game-container')
+const gameSelector = document.getElementById('game-select');
+gameSelector.addEventListener('change',function(){
+   gameContainer.innerHTML = '';
+   switch(gameSelector.value){
+       case 'number-guess':
+           startNumberGuessGame();
+           break;
+       case 'clicker':
+           ClickCounterGame();
+           break;
+   }
+});
 
-//     let button3 = document.createElement("button");
-//     button3.textContent = "リセット";
+ function ClickCounterGame(){
+    let count = 0;
+    const gameContainer = document.getElementById("game-container");
 
-//     let counter = document.createElement("p");
-//     counter.textContent = count;
+    let button1 = document.createElement("button");
+        button1.textContent = "+1ボタン";
 
-//     button1.addEventListener("click",function(){
-//         if(count <= 99){
+    let button2 = document.createElement("button");
+        button2.textContent ="+10ボタン";
+
+    let button3 = document.createElement("button");
+    button3.textContent = "リセット";
+
+    let counter = document.createElement("p");
+     counter.textContent = count;
+
+     button1.addEventListener("click",function(){
+         if(count <= 99){
             
-//         count++;
-//         counter.textContent = count;
-//         }
-//     })
+         count++;
+         counter.textContent = count;
+         }
+     })
 
-//     button2.addEventListener("click",function(){
-//         if(count <= 90){
-//         count = count + 10;
-//         counter.textContent = count;
-//         }
-//     })
+     button2.addEventListener("click",function(){
+         if(count <= 90){
+         count = count + 10;
+         counter.textContent = count;
+         }
+     })
 
-//     button3.addEventListener("click",function(){
-//         count = 0;
-//        counter.textContent = count;
+     button3.addEventListener("click",function(){
+         count = 0;
+        counter.textContent = count;
         
-//     })
+     })
 
-//     gameContainer.appendChild(button1);
-//     gameContainer.appendChild(button2);
-//     gameContainer.appendChild(button3);
-//     gameContainer.appendChild(counter);
-// }
+     gameContainer.appendChild(button1);
+     gameContainer.appendChild(button2);
+     gameContainer.appendChild(button3);
+     gameContainer.appendChild(counter);
+ }
 
-// ClickCounterGame();
 
 function startNumberGuessGame(){
     let count = 0;
@@ -78,4 +108,4 @@ function startNumberGuessGame(){
 
 }
 
-startNumberGuessGame();
+
